220ef775106b9d034ce14b211f06c07134c45ba6
francescocifaldi <francesco.cifaldi@gmail.com>
2025-05-29 17:40:56 +0200

    ADD MovieService

--- BE/mymoviedb/src/main/java/org/lessons/java/mymoviedb/controller/MovieController.java
+++ BE/mymoviedb/src/main/java/org/lessons/java/mymoviedb/controller/MovieController.java
@@ -1,6 +1,6 @@
 package org.lessons.java.mymoviedb.controller;
 
-import org.lessons.java.mymoviedb.repository.MovieRepository;
+import org.lessons.java.mymoviedb.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/movies")
 public class MovieController {
     @Autowired
-    private MovieRepository movieRepository;
+    private MovieService movieService;
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("movies", movieRepository.findAll());
+        model.addAttribute("movies", movieService.findAll());
         return "movies/index";
     }
 
     @GetMapping("{id}")
     public String show(@PathVariable Integer id, Model model) {
-        model.addAttribute("movie", movieRepository.findById(id).get());
+        model.addAttribute("movie", movieService.getById(id));
         return "movies/show";
     }
 
