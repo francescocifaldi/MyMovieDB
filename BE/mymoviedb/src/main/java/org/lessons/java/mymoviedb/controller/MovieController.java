4a8a54fc2f04e592edb700a8ac5e399c09bbb096
francescocifaldi <francesco.cifaldi@gmail.com>
2025-05-29 17:14:35 +0200

    ADD header in fragments and simple show for movie

--- BE/mymoviedb/src/main/java/org/lessons/java/mymoviedb/controller/MovieController.java
+++ BE/mymoviedb/src/main/java/org/lessons/java/mymoviedb/controller/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,4 +19,11 @@ public class MovieController {
         model.addAttribute("movies", movieRepository.findAll());
         return "movies/index";
     }
+
+    @GetMapping("{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        model.addAttribute("movie", movieRepository.findById(id).get());
+        return "movies/show";
+    }
+
 }
