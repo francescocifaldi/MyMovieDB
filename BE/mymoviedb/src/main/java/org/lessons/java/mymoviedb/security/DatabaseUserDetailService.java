41bc7348b2c6ebf6da6763788d4b12c1debc0fdd
francescocifaldi <francesco.cifaldi@gmail.com>
2025-06-12 09:14:33 +0200

    ADD DBUser, DBUserService, SecConfig and UserRepo

---
+++ BE/mymoviedb/src/main/java/org/lessons/java/mymoviedb/security/DatabaseUserDetailService.java
@@ -0,0 +1,27 @@
+package org.lessons.java.mymoviedb.security;
+
+import java.util.Optional;
+
+import org.lessons.java.mymoviedb.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userAttempt = userRepository.findByUsername(username);
+        if (userAttempt.isEmpty()) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return new DatabaseUserDetails(userAttempt.get());
+    }
+
+}
